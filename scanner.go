6ad83094b4b32e0b8ed4f35c1e4251f8069973ba
wux1an <wux1an@outlook.com>
2022-04-09 13:27:04 -0400

    feat: implement scanner

--- scanner.go
+++ scanner.go
@@ -3,11 +3,15 @@ package scanner
 import (
 	"github.com/pkg/errors"
 	"net"
+	"strconv"
+	"sync"
+	"time"
 )
 
 type Config struct {
 	hosts           []net.IP
 	ports           []int
+	mix             bool // mix mode, disable: scan queue like 'host1:port1', 'host1:port2', enable: 'host1:port1', 'host2:port1'
 	thread          int
 	timeoutInSecond int
 }
@@ -30,12 +34,68 @@ func NewConfig(hosts []net.IP, ports []int, thread int, timeoutInSecond int) (*C
 	return &Config{hosts: hosts, ports: ports, thread: thread, timeoutInSecond: timeoutInSecond}, nil
 }
 
-type Scanner struct {
-	config *Config
+type ScanItem struct {
+	host net.IP
+	port int
+	err  error
 }
 
-func NewScanner(config *Config) (*Scanner, error) {
-	// todo implement
+type Scanner struct {
+	config   *Config
+	progress chan *ScanItem
+}
 
-	return nil, nil
+func NewScanner(config *Config) *Scanner {
+	return &Scanner{
+		config:   config,
+		progress: make(chan *ScanItem),
+	}
+}
+
+func (s *Scanner) Progress() chan<- *ScanItem {
+	return s.progress
+}
+
+func (s Scanner) Scan() {
+	if s.config.mix {
+		for _, port := range s.config.ports {
+			for _, host := range s.config.hosts {
+				s.Scan0(&ScanItem{
+					host: host,
+					port: port,
+				})
+			}
+		}
+	} else {
+		for _, host := range s.config.hosts {
+			for _, port := range s.config.ports {
+				s.Scan0(&ScanItem{
+					host: host,
+					port: port,
+				})
+			}
+		}
+	}
+}
+
+func (s Scanner) Scan0(item *ScanItem) {
+	var wg sync.WaitGroup
+	var ch = make(chan interface{}, s.config.thread)
+
+	ch <- nil
+	wg.Add(1)
+	go func() {
+		defer func() {
+			<-ch
+			wg.Done()
+		}()
+
+		_, item.err = net.DialTimeout("tcp",
+			net.JoinHostPort(item.host.String(), strconv.Itoa(item.port)),
+			time.Duration(s.config.timeoutInSecond)*time.Second,
+		)
+	}()
+
+	wg.Wait()
+	close(s.progress)
 }
